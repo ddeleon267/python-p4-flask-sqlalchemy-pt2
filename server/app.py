2bbaa796329ac71a6d1f9148ae4f59435dc70db7
Dominique DeLeon <dominique.a.deleon@gmail.com>
2023-01-27 17:04:35 -0500

    done

--- server/app.py
+++ server/app.py
@@ -1,18 +1,77 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, make_response
 from flask_migrate import Migrate
+
+from models import db, Pet, Owner
 
 from models import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+#Our app.config is set up to point to our existing database, and 'SQLALCHEMY_TRACK_MODIFICATIONS' 
+#is set to False to avoid building up too much unhelpful data in memory when our application is running.
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
+# Our migrate instance configures the application and models for Flask-Migrate
 
 db.init_app(app)
+# db.init_app connects our database to our application before it runs.
 
+#@app.route determines which resources are available at which URLs and saves them to the application's URL map.
+@app.route('/')
+def index():
+    response = make_response(
+        '<h1>Welcome to the pet/owner directory!</h1>',
+        200
+    )
+    return response
+
+@app.route('/pets/<int:id>')
+def pet_by_id(id):
+    pet = Pet.query.filter(Pet.id == id).first()
+
+    if not pet:
+        response_body = '<h1>404 pet not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+
+    response_body = f'''
+        <h1>Information for {pet.name}</h1>
+        <h2>Pet Species is {pet.species}</h2>
+        <h2>Pet Owner is {pet.owner.name}</h2>
+    '''
+
+    response = make_response(response_body, 200)
+
+    return response
+
+@app.route('/owner/<int:id>')
+def owner_by_id(id):
+    owner = Owner.query.filter(Owner.id == id).first()
+
+    if not owner:
+        response_body = '<h1>404 owner not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+
+    response_body = f'<h1>Information for {owner.name}</h1>'
+
+    pets = [pet for pet in owner.pets]
+
+    if not pets:
+        response_body += f'<h2>Has no pets at this time.</h2>'
+
+    else:
+        for pet in pets:
+            response_body += f'<h2>Has pet {pet.species} named {pet.name}.</h2>'
+
+    response = make_response(response_body, 200)
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+
